964d535627f00850c9ef45319f0ce55dd08568d4
santi-estilla20 <os967577@gmail.com>
2026-04-07 14:57:41 -0600

    Update Ejercicio 15 IF.c

--- Ejercicio 15 IF.C
+++ Ejercicio 15 IF.C
@@ -1,1 +1,27 @@
-#include (st
+#include <stdio.h>
+
+int main()
+{
+    int pr = 0;
+    int np = 0;
+    
+    printf("Este programa calcula el precio total de platillos que ofrece una empresa de comida\n");
+    printf("Ingrese el número de personas: ");
+    scanf("%d",&np);
+    
+    if (np > 200)
+    {
+        if (np <= 300)
+            pr = np * 85;
+        else
+            pr = np * 75;
+    }
+    else
+    {
+        pr = np * 95;
+    }
+        
+    printf("El costo total es: %d", pr);
+
+    return 0;
+}
